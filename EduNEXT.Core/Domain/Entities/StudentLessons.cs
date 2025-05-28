00a4a2efef60a1ae119afc756b74a3351e2c8c92
Grigorii Kokotkin <padgamerrfp@gmail.com>
2025-05-28 23:14:29 +0400

    1. Added entities

---
+++ EduNEXT.Core/Domain/Entities/StudentLessons.cs
@@ -0,0 +1,12 @@
+﻿namespace EduNEXT.Core.Domain.Entities;
+
+public class StudentLessons
+{
+    public Guid LessonId { get; set; }
+    
+    public Guid StudentId { get; set; }
+    
+    public Lesson? Lesson { get; set; }
+    
+    public Student? Student { get; set; }
+}
---
+++ EduNEXT.Core/Domain/Entities/TimeSlot.cs
@@ -0,0 +1,14 @@
+﻿namespace EduNEXT.Core.Domain.Entities;
+
+public class TimeSlot
+{
+    public Guid Id { get; set; }
+    
+    public Guid? StudentId { get; set; }
+    
+    public bool IsAvailable { get; set; }
+    
+    public DateTime StartTime { get; set; }
+    
+    public DateTime EndTime { get; set; }
+}
