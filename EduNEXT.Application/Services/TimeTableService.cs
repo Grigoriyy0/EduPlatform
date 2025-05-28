28610780bda4b42bf2d42478602cf3f28cee8ca6
Grigorii Kokotkin <padgamerrfp@gmail.com>
2025-05-29 00:42:08 +0400

    3. reference

---
+++ EduNEXT.Application/Services/TimeTableService.cs
@@ -0,0 +1,12 @@
+﻿using CSharpFunctionalExtensions;
+using EduNEXT.Core.Domain.Entities;
+
+namespace EduNEXT.Application.Services;
+
+public class TimeTableService
+{
+    public async Task<TimeSlot> CreateTimeSlot(TimeSlot timeSlot)
+    {
+        throw new NotImplementedException();
+    }
+}
--- EduNEXT.Core/Domain/Entities/TimeSlot.cs
+++ EduNEXT.Core/Domain/Entities/TimeSlot.cs
@@ -1,7 +1,23 @@
-﻿namespace EduNEXT.Core.Domain.Entities;
+﻿using CSharpFunctionalExtensions;
+
+namespace EduNEXT.Core.Domain.Entities;
 
 public class TimeSlot
 {
+    private TimeSlot(
+        Guid id,
+        Guid? studentId,
+        bool isAvailable,
+        DateTime startTime,
+        DateTime endTime)
+    {
+        Id = id;
+        StudentId = studentId;
+        IsAvailable = isAvailable;
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+    
     public Guid Id { get; set; }
     
     public Guid? StudentId { get; set; }
@@ -11,4 +27,17 @@ public class TimeSlot
     public DateTime StartTime { get; set; }
     
     public DateTime EndTime { get; set; }
+
+    public static Result<TimeSlot, string> Create(
+        Guid id,
+        DateTime startTime,
+        DateTime endTime)
+    {
+        if (startTime > endTime)
+        {
+            return Result.Failure<TimeSlot, string>("Start time can't be after End time");
+        }
+        
+        return new TimeSlot(id, null, true, startTime, endTime);
+    }
 }
